548aa1559f481d80673c08137fcc4ded09822316
Owen Buchheit <obuchheit45@gmail.com>
2024-10-02 23:09:03 -0500

    Started parent class for reading json

--- TPMS_Catch_A_Tail.py
+++ TPMS_Catch_A_Tail.py
@@ -1,8 +1,18 @@
 import json
+import os
 
+#intializes rtl_433
+os.system('rtl_433 -f 315M -F json:test.json -M level -M time -K gpsd,lat,lon')
 
 
 class ID:
   '''Makes a new unique id with a list of permanent items and dict of changable items linked to a Tire Pressure Sensor Monitor.'''
+
+  def __init__(self):
+    with open('cat.json', 'r') as infile:
+      pass
+
+
+
 
   
