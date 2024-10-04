e20272e8871d218ff7abf78639002e90b7fd69c8
Owen Buchheit <obuchheit45@gmail.com>
2024-10-03 22:22:05 -0500

    Reorganized

--- TPMS_Catch_A_Tail.py
+++ TPMS_Catch_A_Tail.py
@@ -6,8 +6,17 @@ import time
 #intializes rtl_433
 #os.system('rtl_433 -f 315M -F csv:cat.csv -M level -M time -K gpsd,lat,lon')
 
+data = {}
+sln = 0
 ln = 0
 
-data = tclass.ID()
-data.get_data()
+with open('test.csv', 'r') as csv_file:
+  csv_reader = csv.DictReader(csv_file)
+
+  for line in csv_reader[sln:]:
+    ln += 1
+    
+    data[line['id']] = [line['time'], line['lat'], line['lon'], line['model'], line['rssi'], line['code']] 
+
+
 
--- TPMS_Classes.py
+++ TPMS_Classes.py
@@ -1,13 +1,5 @@
 import csv
-
-data = {}
-
-with open('test.csv', 'r') as csv_file:
-  csv_reader = csv.DictReader(csv_file)
-
-  for line in csv_reader:
-    data[line['id']] = [line['lat'], line['lon'], line['model'], line['rssi'], line['code']] 
-
+import TPMS_Catch_A_Tail as cat
 
 class UID:
   '''Makes a new unique id with a list of permanent items and dict of changable items linked to a Tire Pressure Sensor Monitor.'''
