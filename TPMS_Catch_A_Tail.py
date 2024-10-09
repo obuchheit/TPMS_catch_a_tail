2507d5677999176452254963ce49dde585495e97
Owen Buchheit <obuchheit45@gmail.com>
2024-10-09 17:30:23 -0500

    Add start index var to avoid duplicative data

--- TPMS_Catch_A_Tail.py
+++ TPMS_Catch_A_Tail.py
@@ -5,59 +5,31 @@ import uuid # might not need it
 
 #intializes rtl_433
 #os.system('rtl_433 -f 315M -F csv:cat.csv -M level -M time -K gpsd,lat,lon')
-ids = []
-objects = []
-
-class ID:
-  def __init__(self, id): 
-    self.id = id
-   
+ids = {}
+startIndex = 0
     
   
 def Main():
   with open('test.csv', 'r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
-    for row in csv_reader:
-      id_dict = {row['id']: [
-        row['time'],
-        float(row['lat']),
-        float(row['lon']),
-        row['model'],
-        float(row['rssi']),
-        int(row['code']),
-        1]}
-      ids.append(id_dict)
-      
-  
-  for key in ids:
-    uid = ID(key)
-    objects.append(uid)
-    
-Main()
-print(objects)
-
-
-
-
-  
-class Id_Manager:
- 
-  def __init__(self):
-    self.ids = []
-    
-  def load_ids(self):
-      with open('test.csv', 'r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        self.ids = [ID(
-          int(row['id']), 
+    for index, row in enumerate(csv_reader):
+      if index >= startIndex:
+        startIndex = startIndex + 1
+        
+        ids[row['id']] = [
+          row['time'],
           float(row['lat']),
           float(row['lon']),
-          row['time'],
           row['model'],
           float(row['rssi']),
-          int(row['code']),)
-              for row in csv_reader ] 
+          int(row['code']),
+          1]
         
+      
+  
+
     
-    #Adds a new ID to the UID class
+Main()
+print(ids)
+
   
