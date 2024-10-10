903a98cc199e2a7dd48c1eedf701e2b83370bac3
Owen Buchheit <obuchheit45@gmail.com>
2024-10-10 12:02:35 -0500

    Add dict methods for manipulating data in test.py

--- test.py
+++ test.py
@@ -7,10 +7,22 @@ import csv
 
 test_id = {'156870': {
   'count': 1,
-  'times': ['']
+  'times': ['0900'],
+  'coords': [(90.001, 49.789)],
+  'RSSI': [-22.6],
+  'model': 'Ford',
+  'code': 180
 }}
+
+
+test_id['156870']['count'] += 1
+test_id['156870']['times'].append('1000')
+test_id['156870']['coords'].append((91, 48))
+
+print(test_id['156870']['count'])
 
 print(test_id)
 
 class UID:
   def __init__(self, id):
+    pass
