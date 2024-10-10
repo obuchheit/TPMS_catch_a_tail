9cdc46415d020716520b7152dd8213eb51a1126d
Owen Buchheit <obuchheit45@gmail.com>
2024-10-10 11:42:00 -0500

    Restructre data

--- TPMS_Catch_A_Tail.py
+++ TPMS_Catch_A_Tail.py
@@ -1,7 +1,6 @@
 import csv
 import os
 import time
-import uuid # might not need it
 
 #intializes rtl_433
 #os.system('rtl_433 -f 315M -F csv:cat.csv -M level -M time -K gpsd,lat,lon')
@@ -16,21 +15,23 @@ def Main():
       if index >= startIndex:
         startIndex = startIndex + 1
         
+        coords = (float(row['lat']), float(row['lat']))
+        variable_data = {count: [row['time'], float(row['lat']), float(row['lon']), float(row['rssi'])]}
         
         if row['id'] in ids:
+          
           pass
         else:
-          ids[row['id']] = [
-            row['time'],
-            float(row['lat']),
-            float(row['lon']),
-            row['model'],
-            float(row['rssi']),
-            int(row['code']),
-            1]
-        
-      
-  
+          ids[row['id']] = {
+            'count': 1,
+            'times': [row['time']],
+            'coords': [coords],
+            'RSSI': [float(row['rssi'])],
+            'model': row['model'],
+            'code': int(row['code'])
+          }
+            
+
 
     
 Main()
--- test.py
+++ test.py
@@ -5,29 +5,12 @@ import csv
     #print(line['time'], line['lat'], line['lon'], line['model'], line['id'],line['rssi'], line['code']) 
 
 
-ids = {}
-ln = 1
+test_id = {'156870': {
+  'count': 1,
+  'times': ['']
+}}
 
-with open('test.csv', 'r') as csv_file:
-  csv_reader = csv.DictReader(csv_file)
-  for row in csv_reader:
-        
-    ids[row['id']] = [
-    row['time'],
-    float(row['lat']),
-    float(row['lon']),
-    row['model'],
-    float(row['rssi']),
-    int(row['code']),
-    1]
-    
+print(test_id)
 
-
-def main():
-    for id in ids:
-      print(id)
-        
-class UID():
-    pass
-
-main()
+class UID:
+  def __init__(self, id):
