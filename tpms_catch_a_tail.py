66dea8652dbb091f16c4688a9829d793b605ad52
obuchheit <obuchheit45@gmail.com>
2024-10-14 19:14:49 -0500

    Add start index functions

--- tpms_catch_a_tail.py
+++ tpms_catch_a_tail.py
@@ -1,11 +1,13 @@
 import csv
 import os
 import time
+import atexit
 
 #intializes rtl_433
 #os.system('rtl_433 -f 315M -F csv:cat.csv -M level -M time -K gpsd,lat,lon')
 
 ids = {}
+
 
 class IDs:
   def __init__(self, id):
@@ -17,19 +19,33 @@ class IDs:
     self.id['coords'].append(coords)
     self.id['RSSI'].append(rssi)
     print(self.id)
-    
-startIndex = 0
+
+'''Reads a text file to give the csv_reader a start index'''
+def read_start_index():
+  if os.path.exists('start_index.txt'):
+    with open('start_index.txt', 'r') as f:
+      return int(f.read().strip())
+  return 0
+
+'''Saves the last index of the csv_reader for loop.'''
+def save_last_index(index):
+  with open('start_index.txt', 'w') as f:
+    f.write(str(index))
+
+'''Deletes the start_index file upon exiting the program'''
+# def delete_txt_file():
+#   os.system("rm start_index.txt")
+
+
+
 '''Loops through csv file from rtl_433 and pushes data into dictionaries'''  
 def Main():
-     
+  startIndex = read_start_index()
+
   with open('test.csv', 'r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
-    print(startIndex)
     for index, row in enumerate(csv_reader):
       if index >= startIndex:
-        
-        
-        #TODO: Figure out a way to += startIndex
         coords = (float(row['lat']), float(row['lon']))
         
         if row['id'] in ids:
@@ -66,6 +82,14 @@ def Main():
           #Instantiates uid class obj
           uids = IDs(id)
 
+          save_last_index(index + 1)
+
+  '''Start other functions from here'''
+
+
+        
+
 Main()
 
 
+atexit.register(delete_txt_file)
