8ab3a77d5b66434998fa996c992c5ae931749af1
obuchheit <obuchheit45@gmail.com>
2025-02-20 21:47:31 -0600

    Delete jsx front-end

--- front-end/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import ReactDom from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import router from './router'
-
-ReactDom.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router}/>
-)
